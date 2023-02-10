1ff0ede4033e52f5a79f605065b96339cf6cb0c8
felman396 <64171681+felman396@users.noreply.github.com>
2023-02-10 14:01:36 -0500

    Index, login and register

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../inicio/**/*.{html,js}"],
+  content: ['../inicio/**/*.{html,js}',
+            '../usuarios/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
