e4867bb3d4d457912a397506e9cff28636988fd6
felman396 <64171681+felman396@users.noreply.github.com>
2023-02-10 15:40:36 -0500

    Solucionando errores

--- EcommerceApp/urls.py
+++ EcommerceApp/urls.py
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-"""EcommerceApp URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("inicio.urls")),
-    path("users/", include("usuarios.urls"))
-]
-=======
 """EcommerceApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -45,8 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("inicio.urls"))
+    path("", include("inicio.urls")),
+    path("users/", include("usuarios.urls"))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 0e1de070cd039d544ec4c4e05f7abe72692d4a8b
+
