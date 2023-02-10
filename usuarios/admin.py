47e498dcafbba80ce3e14a945f3c4f92f85c814d
cilachoque <cilachoque@cilachoque.cilachoque>
2023-02-09 23:20:08 -0500

    Modified entities of database

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuario, Producto, Venta, Detalle_Venta
+
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(Detalle_Venta)
--- usuarios/models.py
+++ usuarios/models.py
@@ -3,6 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    nombre = models.TextField()
-    apellido = models.TextField()
-    edad = models.TextField()
+    nombre = models.CharField(max_length=100)
+    apellido = models.TextField(max_length=100)
+    edad = models.IntegerField()
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    imagen = models.TextField()#falta tipo img
+    precio = models.DecimalField(max_digits=10, decimal_places=3)
+    stock = models.IntegerField()
+    descuento = models.BooleanField(blank=True)
+
+class Venta(models.Model):
+    idUsuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    fecha = models.DateField()
+    total = models.DecimalField(max_digits=10, decimal_places=3)
+
+class Detalle_Venta(models.Model):
+    idVenta = models.ForeignKey('Venta', on_delete=models.CASCADE)
+    idProducto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=3)
