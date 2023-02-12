b9d3ae4391f6061b84d2c231cf5b9f56f3eab908
ChristianIlachoque <cilachoque@unsa.edu.pe>
2023-02-11 21:50:27 -0500

    Alter table Usuario with more atributes

--- usuarios/models.py
+++ usuarios/models.py
@@ -6,6 +6,8 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.TextField(max_length=100)
     edad = models.IntegerField()
+    correo = models.EmailField(max_length=100, default='sin_correo@gmail.com')
+    clave = models.CharField(max_length=50, default='sin_clave')
 
 # class Producto(models.Model):
 #     nombre = models.CharField(max_length=100)
