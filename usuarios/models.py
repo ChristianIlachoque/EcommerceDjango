b82ef2c49cb421bf86802b3d750daa3a6c8d9953
ChristianIlachoque <cilachoque@unsa.edu.pe>
2023-02-14 17:31:51 -0500

    Alter Usuario's atribute -apellido-

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
-    apellido = models.TextField(max_length=100)
+    apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
     correo = models.EmailField(max_length=100, default='sin_correo@gmail.com')
     clave = models.CharField(max_length=50, default='sin_clave')
